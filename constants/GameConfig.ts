95c561da2b808c1e7559b7c1351756dccdaf1374
Inju Michorius <i.michorius@am-impact.nl>
2026-01-21 15:52:29 +0100

    Add some fun tiles

--- constants/GameConfig.ts
+++ constants/GameConfig.ts
@@ -17,25 +17,72 @@ export const BOARD_TILES = [
     icon: "flag-checkered",
     actionType: "modal",
   },
-  // Tested tiles
   {
     id: 2,
+    name: "Ga 2 stappen vooruit",
+    icon: "arrow-right",
+    description: "Hoera! Je mag 2 stappen vooruitlopen.",
+    actionType: "modal",
+    moveAmount: 2,
+  },
+  {
+    id: 3,
     name: "Neem 2 stappen terug",
     icon: "arrow-left",
     description: "Helaas je moet 2 stappen teruglopen.",
-    sipCount: 0,
     actionType: "modal",
     moveAmount: -2,
   },
-  // Add more tiles as needed
   {
-    id: 3,
+    id: 4,
     name: "Slechte voorbereiding",
     icon: "battery-quarter",
     description:
-      "De speler met de laagste telefoonbatterij percentage neemt 5 slokken.",
+      "De speler met de laagste batterij percentage neemt 5 slokken.",
     sipCount: 5,
     actionType: "everyone",
-    moveAmount: 0,
   },
+  {
+    id: 5,
+    name: "Goede voorbereiding",
+    icon: "battery-full",
+    description:
+      "De speler met de hoogste batterij percentage mag 5 slokken uitdelen",
+    sipCount: 5,
+    actionType: "everyone",
+  },
+  {
+    id: 6,
+    name: "Categorieën",
+    icon: "layer-group",
+    description:
+      "Onderdelen in een computer. De speler die in herhaling treed neemt 5 slokken",
+    sipCount: 5,
+    actionType: "everyone",
+  },
+  {
+    id: 7,
+    name: "Spraakhandicap",
+    icon: "comment",
+    description:
+      "Al je zinnen moeten eindigen met het woord 'banaan' tot je volgende beurt. Neem 1 slok als je dit vergeet.",
+    actionType: "modal",
+  },
+  {
+    id: 8,
+    name: "Even goede vrienden",
+    icon: "handshake",
+    description: "Wie als laatste een handdruk geeft drinkt 5 slokken.",
+    sipCount: 5,
+    actionType: "everyone",
+  },
+  {
+    id: 9,
+    name: "Potje vingeren",
+    icon: "fingerprint",
+    description: "Speel een potje vingeren. De verliezer drinkt 5 slokken.",
+    sipCount: 5,
+    actionType: "everyone",
+  },
+  // Add more tiles as needed
 ];
